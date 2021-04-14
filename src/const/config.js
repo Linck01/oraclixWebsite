dc8d301a2243419f9bbc38deeed25fd95932188c
Linck01 <sebashatt@gmail.com>
2021-04-14 03:39:42 +0200

    Basic pages finished.

--- src/const/config.js
+++ src/const/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   defaultPrefix:'??',
-  discordBotInvite:'https://google.com',
+  discordBotInvite:'https://discord.com/api/oauth2/authorize?client_id=813091242325835817&permissions=2148531392&scope=bot',
   discordSupportServer:'https://discord.gg/GUz3dDnr5X'
 };
